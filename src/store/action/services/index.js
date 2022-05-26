458c6758e1cf89b8e650cbce24de0f68e1a3f050
Cedric Nathan Legarda <31382654+cedric28@users.noreply.github.com>
2022-05-27 01:24:19 +0800

    update service action

--- src/store/action/services/index.js
+++ src/store/action/services/index.js
@@ -2,7 +2,7 @@ import { servicesActionType } from '../../constants/actionType';
 import { axiosInstance } from '../../util/axios';
 
 export const addServices = serviceData => (dispatch /* , getState */) => {
-  return axiosInstance('post', '/services/add', serviceData)
+  return axiosInstance('post', 'services/add', serviceData)
     .then(res => res.data)
     .then(res => {
       if (res.success) {
@@ -15,7 +15,7 @@ export const addServices = serviceData => (dispatch /* , getState */) => {
 };
 
 export const fetchServices = () => (dispatch, getState) => {
-  return axiosInstance('get', '/services/fetch')
+  return axiosInstance('get', 'services/fetch')
     .then(res => res.data)
     .then(res => {
       if (res.success) {
